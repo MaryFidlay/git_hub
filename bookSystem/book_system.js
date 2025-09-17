e19aa67e1afbbd3a8c083399241b3f8a28e0cc3f
Maria <mariefidlay@gmail.com>
2025-09-16 23:38:15 -0400

    task

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -5,6 +5,7 @@ function addBook() {
     const authorName = document.getElementById('authorName').value;
     const bookDescription = document.getElementById('bookDescription').value;
     const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+
     if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
         const book = {
             name: bookName,
@@ -21,13 +22,16 @@ function addBook() {
 }
 
 function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
-        <p><strong>Book Name: </strong>${book.name}</p>
+    const booksDiv = books.map((book, index) => `
+        <h1>Book Number: ${index + 1}</h1>
+        <p><strong>Book Name:</strong> ${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
-    );
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deleteBook(${index})">Delete</button>
+        <hr>
+    `);
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
 
@@ -37,11 +41,18 @@ function editbook(index) {
     document.getElementById('authorName').value = book.authorName;
     document.getElementById('bookDescription').value = book.bookDescription;
     document.getElementById('pagesNumber').value = book.pagesNumber;
-    books.splice(index, 1); // Remove old entry
-    showbooks(); // Refresh list
-  }
 
-  function clearInputs() {
+   
+    books.splice(index, 1);
+    showbooks();
+}
+
+function deleteBook(index) {
+    books.splice(index, 1); 
+    showbooks();
+}
+
+function clearInputs() {
     document.getElementById('bookName').value = '';
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
