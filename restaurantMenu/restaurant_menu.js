d229c774500d8bfc332a99a0632655dcd00c465b
Maria <mariefidlay@gmail.com>
2025-09-16 22:17:58 -0400

    practice_task

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,14 +1,14 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $28', 'Pasta - $22', 'Burger - $18', 'Salmon - $25'];
+const dessertMenu = ['Cake - $8', 'Ice Cream - $6.5', 'Pudding - $7', 'Fruit Salad - $5.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
-
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
-
 let mainCourseItem = '';
-mainCourseMenu.forEach((item, index) => {
-mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+mainCourseMenu.forEach((item, index) => {mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
 document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
