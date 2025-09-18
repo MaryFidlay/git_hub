974a385a78d0ec2af4232873f3302744e407e82e
Maria <maryfidlay@gmail.com>
2025-09-18 11:13:54 -0400

    taks

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -34,6 +34,8 @@ startTime = new Date().getTime();
 
             var wpm = 0; // Default value
 
+            var charCount = userTypedText.length;
+
             if (timeElapsed !== 0 && !isNaN(typedWords)) {
                 wpm = Math.round((typedWords / timeElapsed) * 60);
             }
@@ -41,7 +43,8 @@ startTime = new Date().getTime();
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
-                "<p>Words Typed: " + typedWords + "</p>" +
+            "<p>Total Length: " + charCount + "</p>" +     
+            "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
             }
